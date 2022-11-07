e90a2d813572c45bd84b72115eb2900c8860ca46
Ivan Krstic <krstic8687@yahoo.com>
2022-11-07 15:07:01 +0100

    added HTTP GET with Spring RestTemplate

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # mssc-brewery-client
+
+## How to run
+
+In order to run this app **mssc-brewery** project should be up and running
---
+++ src/test/java/it/springframework/msscbreweryclient/web/client/BreweryClientTest.java
@@ -0,0 +1,27 @@
+package it.springframework.msscbreweryclient.web.client;
+
+import it.springframework.msscbreweryclient.web.model.BeerDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/*
+*   In order to run this test mssc-brewery project must be running
+* */
+@SpringBootTest //bring up spring context
+class BreweryClientTest {
+
+    @Autowired
+    BreweryClient client;
+
+    @Test
+    void getBeerById() {
+        BeerDto beerDto = client.getBeerById(UUID.randomUUID());
+
+        assertNotNull(beerDto);
+    }
+}
