93c68377d0049e838a635cec537cee82ec78da18
Ivan Krstic <krstic8687@yahoo.com>
2022-11-07 15:15:13 +0100

    added HTTP POST with Spring RestTemplate

--- src/test/java/it/springframework/msscbreweryclient/web/client/BreweryClientTest.java
+++ src/test/java/it/springframework/msscbreweryclient/web/client/BreweryClientTest.java
@@ -5,12 +5,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.net.URI;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 /*
-*   In order to run this test mssc-brewery project must be running
+*   In order to run this integration test mssc-brewery project must be running
 * */
 @SpringBootTest //bring up spring context
 class BreweryClientTest {
@@ -23,5 +24,16 @@ class BreweryClientTest {
         BeerDto beerDto = client.getBeerById(UUID.randomUUID());
 
         assertNotNull(beerDto);
+    }
+
+    @Test
+    void testSaveNewBeer() {
+        BeerDto beerDto = BeerDto.builder().beerName("New_Beer").build();
+
+        URI uri = client.saveNewBeer(beerDto);
+
+        assertNotNull(uri);
+
+        System.out.println(uri.toString());
     }
 }
